030216cf8a4b3c98fb4e9f40521f568c443661b2
David Parvin <dwparvin@cableone.net>
2021-07-11 19:21:25 -0600

    Add project files.

---
+++ VerifyErrorReproProjectCS/BasicItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace VerifyErrorReproProjectCS
+{
+    public class BasicItem
+    {
+
+        public BasicItem()
+        {
+            Name = string.Empty;
+            Type = 0;
+            Setting = new XElement("root");
+        }
+
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public XElement Setting { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name = \"{Name}\", Type = {Type}, Setting = {Setting}";
+        }
+    }
+}
---
+++ VerifyErrorReproProjectCSTest/VerifyErrorReproProjectCSTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+using VerifyErrorReproProjectCS;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace VerifyErrorReproProjectCSTest
+{
+    public class BasicItemTest
+    {
+
+        private ITestOutputHelper Output { get; set; }
+
+        public BasicItemTest(ITestOutputHelper outputHelper) : base()
+        {
+            Output = outputHelper;
+        }
+
+        [Fact]
+        public void TestOfBasicItem()
+        {
+            var obj = new BasicItem
+            {
+                Name = "TestItem",
+                Type = 55
+            };
+            obj.Setting.Add(new XElement("TestItem"));
+            Output.WriteLine("obj: " + obj.ToString());
+        }
+    }
+}
