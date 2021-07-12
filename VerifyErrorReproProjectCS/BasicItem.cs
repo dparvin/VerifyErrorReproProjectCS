1b93f4ad22dc06c6de1dcea18c4adc2cc7db395e
David Parvin <dwparvin@cableone.net>
2021-07-11 21:10:32 -0600

    Change the projects to be an sdk type

--- VerifyErrorReproProjectCS/BasicItem.cs
+++ VerifyErrorReproProjectCS/BasicItem.cs
@@ -2,9 +2,15 @@
 
 namespace VerifyErrorReproProjectCS
 {
+    /// <summary>
+    /// Basic Item
+    /// </summary>
     public class BasicItem
     {
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicItem"/> class.
+        /// </summary>
         public BasicItem()
         {
             Name = string.Empty;
@@ -12,10 +18,34 @@ namespace VerifyErrorReproProjectCS
             Setting = new XElement("root");
         }
 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
         public int Type { get; set; }
+        /// <summary>
+        /// Gets or sets the setting.
+        /// </summary>
+        /// <value>
+        /// The setting.
+        /// </value>
         public XElement Setting { get; set; }
 
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return $"Name = \"{Name}\", Type = {Type}, Setting = {Setting}";
--- VerifyErrorReproProjectCSTest/BasicItemTest.cs
+++ VerifyErrorReproProjectCSTest/BasicItemTest.cs
@@ -1,23 +1,44 @@
 ﻿using System.Xml.Linq;
 using VerifyErrorReproProjectCS;
-using VerifyTests;
-using VerifyXunit;
 using Xunit;
 using Xunit.Abstractions;
 
 
 namespace VerifyErrorReproProjectCSTest
 {
-    public class BasicItemTest : VerifyBase
+    /// <summary>
+    /// Test of a Basic Item
+    /// </summary>
+    ///// <seealso cref="VerifyBase" />
+    public class BasicItemTest //: VerifyBase
     {
-        private VerifySettings Settings { get; set; }
+        ///// <summary>
+        ///// Gets or sets the settings.
+        ///// </summary>
+        ///// <value>
+        ///// The settings.
+        ///// </value>
+        //private VerifySettings Settings { get; set; }
+        /// <summary>
+        /// Gets or sets the output.
+        /// </summary>
+        /// <value>
+        /// The output.
+        /// </value>
         private ITestOutputHelper Output { get; set; }
 
-        public BasicItemTest(ITestOutputHelper outputHelper) : base()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicItemTest"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The output helper.</param>
+        public BasicItemTest(ITestOutputHelper outputHelper) //: base()
         {
             Output = outputHelper;
         }
 
+        /// <summary>
+        /// Tests the of basic item.
+        /// </summary>
         [Fact]
         public void TestOfBasicItem()
         {
@@ -28,11 +49,16 @@ namespace VerifyErrorReproProjectCSTest
             };
             obj.Setting.Add(new XElement("TestItem"));
             Output.WriteLine("obj: " + obj.ToString());
-            VerifyObject(obj);
+            //VerifyObject(obj);
         }
-        private async void VerifyObject(object Item)
-        {
-            await Verify(Item, Settings);
-        }
+
+        ///// <summary>
+        ///// Verifies the object.
+        ///// </summary>
+        ///// <param name="Item">The item.</param>
+        //private async void VerifyObject(object Item)
+        //{
+        //    await Verify(Item, Settings);
+        //}
     }
 }
